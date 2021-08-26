38797372b3c8645d4efc10a13ec1406b8bd74eb5
marceline-cramer <cramermarceline@gmail.com>
2021-08-26 15:18:03 -0600

    Reset to blank init.vim

--- nvim/lua/plugins/filetree.lua
+++
@@ -1,3 +0,0 @@
-local map = vim.api.nvim_set_keymap
-
-map('n', ';e', ':NvimTreeToggle<CR>', { noremap = true })
--- nvim/lua/plugins/lspkind.lua
+++
@@ -1,9 +0,0 @@
-require('lspkind').init({
-    with_text = false,
-    preset = 'default',
-    symbol_map = {
-      Field = "",
-      Class = "",
-      Property =  "",
-    }
-})
